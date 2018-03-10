c10c203e16a03e1d0b1cde62c76f1f2c155da9d8
Sayan Mukherjee <sayan.mukherjee108@gmail.com>
2018-03-09 20:30:13 -0500

    fully functional storing and caching

--- user.js
+++ user.js
@@ -1,24 +1,35 @@
 const bluebird = require("bluebird");
 const redis = require("redis");
+const flat = require("flat");
+const unflatten = flat.unflatten;
 const redisClient = redis.createClient();
 const UserData = require('./user_data.json');
+const MAX_CACHE_LENGTH = 20;
 
 bluebird.promisifyAll(redis.RedisClient.prototype);
 bluebird.promisifyAll(redis.Multi.prototype);
 
 const getById = async (id) => {
-    // 1. check redis for user
-
-    // 2. check store for user
-    const user = UserData.filter(user => {
-        return user.id === parseInt(id);
-    });
-
+    // check redis for user
+    let user;
+    
     try {
-     // 3. cache user in redis or return {}
-    const jsonUser = JSON.stringify(user);
-    let redisUserCache;
-    redisUserCache = await redisClient.rpush("users", jsonUser);
+        // check store for user
+        user = UserData.filter(user => {
+            return user.id === parseInt(id);
+        });
+
+        // cache user in redis or return {}
+        if(typeof user !== "undefined" && user.length > 0) {
+            const cacheLength = await redisClient.llenAsync("users");
+            
+            if (cacheLength > MAX_CACHE_LENGTH - 1) { // limit redis cache size 
+                await redisClient.lpopAsync("users"); // removes first entry in list
+            }
+            
+            const jsonUser = JSON.stringify(user);
+            let redisUserCache = await redisClient.rpush("users", jsonUser);
+        }
     } catch (error) {
         console.log(error);
     }
@@ -30,8 +41,7 @@ const getById = async (id) => {
 const getHistory = async () => {
     let cachedUsers;
     try {
-        cachedUsers = await redisClient.lrangeAsync("users", 0, 19);
-        cachedUsers = JSON.parse(cachedUsers);
+        cachedUsers = await redisClient.lrangeAsync("users", 0, MAX_CACHE_LENGTH - 1);
     } catch (error) {
         console.log(error);
     }
