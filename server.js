f9c0a79e1839e25298e2c0b844abd4736649f513
Sayan Mukherjee <sayan.mukherjee108@gmail.com>
2018-03-09 20:01:44 -0500

    working redis cache

--- server.js
+++ server.js
@@ -3,14 +3,33 @@ const app = express();
 const user = require("./user.js");
 //const UserData = require('./user_data.json');
 
-app.get("/api/people/:id", async (req, res) => {
-    const data = user.getById(req.params.id);
-    console.log(data);
-    res.status(200).json(data);
+app.get('/api/people/history', async (req, res) => {
+    try {
+        const history = await user.getHistory();
+        if (typeof history !== "undefined" && history.length > 0) {
+            res.status(200).json(history);
+        } else {
+            res.status(404).json({error: "History is written by victors!"});
+        }
+    } catch (err) {
+        res.status(500);
+        console.log("Error /api/people/history " + "\n" + err);
+    }
 });
 
-app.get('/api/people/history', async (req, res) => {
-    res.status(200);
+app.get("/api/people/:id", async (req, res) => {
+    try {
+        const data = await user.getById(req.params.id);
+        if (data.length > 0) {
+            res.status(200).json(data[0]);
+        } else {
+            res.status(404).json({error: "Record Not Found"});
+        }
+    } catch (err) {
+        // the server gave up!
+        res.status(500).send();
+        console.log("Error /api/people/:id" + '/n' + err);
+    }
 });
 
 app.get('*', (req, res) => {
--- user.js
+++ user.js
@@ -3,21 +3,40 @@ const redis = require("redis");
 const redisClient = redis.createClient();
 const UserData = require('./user_data.json');
 
-const getById = (id) => {
+bluebird.promisifyAll(redis.RedisClient.prototype);
+bluebird.promisifyAll(redis.Multi.prototype);
+
+const getById = async (id) => {
     // 1. check redis for user
 
     // 2. check store for user
-
-    // 3. cache user in redis or return {}
     const user = UserData.filter(user => {
         return user.id === parseInt(id);
     });
 
+    try {
+     // 3. cache user in redis or return {}
+    const jsonUser = JSON.stringify(user);
+    let redisUserCache;
+    redisUserCache = await redisClient.rpush("users", jsonUser);
+    } catch (error) {
+        console.log(error);
+    }
+
     return user;
 };
 
-const getHistory = () => {
-    // return redis cache [limit 20]
+// return redis cache [limit 20]
+const getHistory = async () => {
+    let cachedUsers;
+    try {
+        cachedUsers = await redisClient.lrangeAsync("users", 0, 19);
+        cachedUsers = JSON.parse(cachedUsers);
+    } catch (error) {
+        console.log(error);
+    }
+
+    return cachedUsers;
 };
 
 module.exports = {
